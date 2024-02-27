edd9e2eeafd70fbaa4c4698cdf0f1b696a8d28ad
Nabil <hafednabil71@gmail.com>
2024-02-27 09:47:49 +0100

    vvv

---
+++ src/test/java/LineCoverageTest/Exo1Test.java
@@ -0,0 +1,30 @@
+package LineCoverageTest;
+
+import org.exemple.Palindrome;
+import org.exemple.PalindromeCorrection;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Exo1Test {
+    private Palindrome   palindrome ;
+    @BeforeEach
+    public void setup(){
+        palindrome = new Palindrome() ;
+    }
+    @Test
+    public void isPalindromeTestNull(){
+        boolean results  = palindrome.isPalindrome(null) ;
+     }
+     @Test
+    public void isPalindrome(){
+        boolean results  = palindrome.isPalindrome("aaaaaa");
+        Assertions.assertEquals(true,results);
+     }
+    @Test
+    public void isPalindromeCorrection(){
+        PalindromeCorrection palindromeCorr = new PalindromeCorrection() ;
+        boolean results  = palindromeCorr.isPalindrome("aaaaaa");
+        Assertions.assertEquals(true,results);
+    }
+}
