f76b5b8988307027f0cbf688e6022b4e221f1285
Nabil <hafednabil71@gmail.com>
2024-03-21 01:01:36 +0100

    vvv

--- src/test/java/LineCoverageTest/Exo2Test.java
+++ src/test/java/LineCoverageTest/Exo2Test.java
@@ -2,6 +2,7 @@ package LineCoverageTest;
 
 
 import org.exemple.Anagram;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +12,21 @@ public class Exo2Test {
     public void upset(){
         anagram = new Anagram()  ;
     }
+    @Test
+    public void isAnagramTestNULL(){
+        Assertions.assertThrows(NullPointerException.class, () -> anagram.isAnagram(null,null));
+    }
+    @Test
+    public void isAnagramTestDfrLength(){
+        boolean results = anagram.isAnagram("abc","ab") ;
+        Assertions.assertFalse(results);
+    }
+    @Test
+    public void isAnagramTestAnagram(){
+        boolean results = anagram.isAnagram("abcd","bdca") ;
+        Assertions.assertTrue(results);
 
+    }
 
 
 }
