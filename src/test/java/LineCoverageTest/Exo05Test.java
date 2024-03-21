74d2e00aad99449a0ff8c131d3a617cbcf7fd88d
Nabil <hafednabil71@gmail.com>
2024-03-21 01:13:58 +0100

    vvv

---
+++ src/test/java/LineCoverageTest/Exo05Test.java
@@ -0,0 +1,24 @@
+package LineCoverageTest;
+
+import org.exemple.QuadraticEquation;
+import org.exemple.RomanNumeral;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Exo05Test {
+    private RomanNumeral romanNumeral  ;
+    @BeforeEach
+    public void setUp(){
+        romanNumeral = new RomanNumeral() ;
+    }
+    @Test
+    public void IllegalArgumentExceptionTest(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> romanNumeral.toRoman(5000));
+    }
+    @Test
+    public void RomanNumeral(){
+
+    }
+
+}
